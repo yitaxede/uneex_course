5a4ca01eafd30453a688a42ca43eb4533b5361bd
user <user@localhost.localdomain>
2019-03-21 10:08:09 -1100

    used pack geometry manager for a frame

--- example03.py
+++ example03.py
@@ -25,11 +25,10 @@ TKroot = Tk()
 TKroot.title("Hello")
 
 root = Frame(TKroot)
-root.place(relx=0, rely=0, relheight=1, relwidth=1)
+root.pack(fill=BOTH)
 
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=2)
-#root.rowconfigure(0, weight=1)
 
 
 Butt = Button(root, text="Add")
@@ -39,4 +38,3 @@ Exit = Button(root, text="Exit", command=root.quit)
 Exit.grid(row=0, column=1, sticky=E+W+S+N)
 
 TKroot.mainloop()
-print("Done")
